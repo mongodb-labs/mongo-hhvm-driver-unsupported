b7d39a0240881f7b33d4adc1716daad21a5e3508
Yanbin Feng <yf225@cornell.edu>
2014-03-19 00:06:12 -0400

    Added MongocCursor wrapper class to mongo_common.cpp/.h

--- src/mongo_common.cpp
+++ src/mongo_common.cpp
@@ -2,6 +2,8 @@
 
 namespace HPHP {
 
+////////MongocClient
+
 MongocClient *MongocClient::GetPersistent(const String& uri) {
   return GetCachedImpl("mongo::persistent_clients", uri);
 }
@@ -25,6 +27,28 @@ MongocClient::MongocClient(const String &uri) {
 MongocClient::~MongocClient() {
   if (m_client != nullptr) {
     mongoc_client_destroy(m_client);
+  }
+}
+
+////////MongocCursor
+
+MongocCursor::MongocCursor(mongoc_client_t           *client,
+                const char                *db_and_collection,
+                mongoc_query_flags_t       flags,
+                uint32_t                   skip,
+                uint32_t                   limit,
+                uint32_t                   batch_size,
+                bool                       is_command,
+                const bson_t              *query,
+                const bson_t              *fields,
+                const mongoc_read_prefs_t *read_prefs) {
+  m_cursor = _mongoc_cursor_new(client, db_and_collection, flags, skip, limit, batch_size,
+                               is_command, query, fields, read_prefs);
+}
+
+MongocCursor::~MongocCursor() {
+  if (m_cursor != nullptr) {
+    mongoc_cursor_destroy (m_cursor);
   }
 }
 
--- src/mongo_common.h
+++ src/mongo_common.h
@@ -32,6 +32,34 @@ private:
 
 };
 
+class MongocCursor : public SweepableResourceData {
+public:
+  //Reference: https://github.com/mongodb/mongo-c-driver/blob/e6038636bcee5264a264b54afce0b93c39884d97/src/mongoc/mongoc-cursor.c
+  MongocCursor(mongoc_client_t           *client,
+                const char                *db_and_collection,
+                mongoc_query_flags_t       flags,
+                uint32_t                   skip,
+                uint32_t                   limit,
+                uint32_t                   batch_size,
+                bool                       is_command,
+                const bson_t              *query,
+                const bson_t              *fields,
+                const mongoc_read_prefs_t *read_prefs);
+  ~MongocCursor();
+
+  CLASSNAME_IS("mongoc cursor")
+
+  // overriding ResourceData
+  virtual const String& o_getClassNameHook() const { return classnameof(); }
+  virtual bool isInvalid() const { return m_cursor == nullptr; }
+
+  mongoc_cursor_t *get() { return m_cursor;}
+
+private:
+  mongoc_cursor_t *m_cursor;
+
+};
+
 }
 
 #endif // incl_HPHP_EXT_MONGO_COMMON_H_
