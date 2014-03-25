aacb1d5ff3e009edc66c95efb09a22cf253706cd
Alisa Nguyen <alisan011@gmail.com>
2014-03-25 16:23:16 -0400

    Implemented MongoDB class functions that deal with only private fields or can call native command function. Still working

--- test/mongoDBTest.php
+++ test/mongoDBTest.php
@@ -7,6 +7,8 @@ class MongoDBTest extends MongoTestCase {
 		$cli = new MongoClient();
 		$database_name = "Fake database";
 		$db = new MongoDB($cli, $database_name);
+		var_dump($db->setSlaveOkay());
+		var_dump($db->getSlaveOkay());
 		printf("Ended testMongoDB\n\n");
 	}
 }
