df48f4143327cdf93fed22818a73727fd01cb459
Developer <developer@developer-VirtualBox.(none)>
2014-03-25 20:51:20 -0400

    Added gloadsvisitor for primitive data types

--- src/bson_php.cpp
+++ src/bson_php.cpp
@@ -11,3 +11,13 @@ cbson_loads_visit_int64 (const bson_iter_t *iter,
   
   return false;
 }
+
+
+static const bson_visitor_t gLoadsVisitors = {
+   .visit_double = cbson_loads_visit_double,
+   .visit_utf8 = cbson_loads_visit_utf8,
+   .visit_array = cbson_loads_visit_array,
+   .visit_bool = cbson_loads_visit_bool,
+   .visit_int64 = cbson_loads_visit_int64;
+};
+
