f9980e4720342ee2cce0d9fd74ee27efb41ab136
Developer <developer@developer-VirtualBox.(none)>
2014-03-25 20:59:04 -0400

    change return type to void

--- src/bson_php.cpp
+++ src/bson_php.cpp
@@ -4,15 +4,33 @@
 static bool
 cbson_loads_visit_int64 (const bson_iter_t *iter,
                          const char*        key,
-                         int64_t          value,
-                         Array*             *output)
+                         int64_t          v_int64,
+                         void             *output)
 {
-  output->add(String(key),value);
+  ((Array *)output)->add(String(key),v_int64);
   
   return false;
 }
 
+static bool
+cbson_loads_visit_bool (const bson_iter_t *iter,
+                        const char        *key,
+                        bool        v_bool,
+                        void       *output)
+{
+   ((Array *)output)->add(String(key), v_bool);
+   return false;
+}
 
+
+static bool
+cbson_loads_visit_array (const bson_iter_t *iter,
+                        const char        *key,
+                        const bson_t     v_array,
+                        void           *output)
+{
+   return false;
+}
 static const bson_visitor_t gLoadsVisitors = {
    .visit_double = cbson_loads_visit_double,
    .visit_utf8 = cbson_loads_visit_utf8,
