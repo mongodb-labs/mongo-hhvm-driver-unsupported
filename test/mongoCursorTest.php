eab29e71e484528830f9b896459f3ce1fdfd73a6
Yanbin Feng <yf225@cornell.edu>
2014-04-02 17:24:57 -0400

    Implemented current/reset/rewind/valid; Deleted test/mongocursor-constructor.php and moved all cursor test cases to mongoCursorTest.php

--- test/mongoCursorTest.php
+++ test/mongoCursorTest.php
@@ -8,11 +8,23 @@ class MongoCursorTest extends MongoTestCase {
 		$database_name = "test.students";
 		$cursor = new MongoCursor(	$cli, 
 									$database_name, 
-									array("test_field" => "1"),
+									array("name" => "Bob"),
                                   	array()	);
-		var_dump($cursor->info());
+		//var_dump($cursor->info());
 		//$this->assertEquals(true, $cursor);
 		//var_dump($cursor->batchSize(5));
+		
+		$cursor->rewind();
+		
+		while ($cursor->valid())
+		{
+		    //$key = $cursor->key();   //This should be tested after BSON-PHP decoder is finished
+		    //var_dump($key);
+		    $value = $cursor->current();
+		    var_dump($value);
+		    $cursor->next();
+		}
+		
 		printf("Ending %s\n\n", __FUNCTION__);
 	}
 
