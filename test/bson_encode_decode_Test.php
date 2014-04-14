050813d05a78a7ee9092aec4021a105b757997d8
Marco Gentili <mgentili@gmail.com>
2014-04-14 14:21:16 -0700

    Added framework for basic encode/decode tests

---
+++ test/bson_encode_decode_Test.php
@@ -0,0 +1,51 @@
+<?php
+
+class DecodingTest extends PHPUnit_Framework_TestCase {
+
+	public function testDecoding() {
+		printf("Starting %s\n", __FUNCTION__);
+		echo "\nTesting string type with valid length prefix\n";
+		$bson  = pack('C', 0x02);                      // byte: string type
+		$bson .= pack('a*x', 'x');                     // cstring: field name
+		$bson .= pack('V', 1);                         // int32: string length (valid)
+		$bson .= pack('a*x', '');                      // cstring: string value
+		$bson .= pack('x');                            // null byte: document terminator
+		$bson  = pack('V', 4 + strlen($bson)) . $bson; // int32: document length
+
+		//$this->assertTrue(bson_decode($bson) == array(["x"] => ""));
+
+		echo "\nTesting string type\n";
+		$bson = pack('C', 0x02); // byte: string type
+		$bson .= pack('a*x', 'x'); // cstring: field name
+		$bson .= pack('V', 0); // int32: string length (invalid)
+		$bson .= pack('a*x', ''); // cstring: string value
+		$bson .= pack('x'); // null byte: document terminator
+		$bson = pack('V', 4 + strlen($bson)) . $bson; // int32: document length
+		//var_dump(bson_decode($bson));
+
+		printf("Ending %s\n", __FUNCTION__);
+	}
+
+	public function testEncodeDecode() {
+		printf("Starting %s\n", __FUNCTION__);
+		$a1 = array("hello" => "world");
+		//$this->assertTrue(bson_decode(bson_encode($a1)) == $a1);
+
+		$id = new MongoId();
+		$a2 = array("_id" => $id);
+		//$this->assertTrue(bson_decode(bson_encode($a2)) == $a2);
+
+		$int32 = new MongoInt32("32");
+		$a2["a_int32"] = $int32;
+		//$this->assertTrue(bson_decode(bson_encode($a2)) == $a2);
+
+		$int64 = new MongoInt64("64");
+		$a2["a_int64"] = $int64;
+		//$this->assertTrue(bson_decode(bson_encode($a2)) == $a2);
+
+		$a3 = array("nested" => $a1);
+		//$this->assertTrue(bson_decode(bson_encode($a3)) == $a3);
+
+		printf("Ending %s\n", __FUNCTION__);
+	}
+} 	
