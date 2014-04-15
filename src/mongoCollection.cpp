70743a8e01c4b6a67fb033ff024c71d68e434b04
Yanbin Feng <yf225@cornell.edu>
2014-04-15 19:48:23 -0400

    Implemented basic version of insert and remove, currently supporting only "name" as key

--- src/mongoCollection.cpp
+++ src/mongoCollection.cpp
@@ -2,6 +2,18 @@
 
 namespace HPHP {
 
+static mongoc_collection_t *get_collection(Object obj) {
+  mongoc_collection_t *collection;
+
+  auto db = obj->o_realProp("db", ObjectData::RealPropUnchecked, "MongoCollection")->toObject();
+  auto client = db->o_realProp("client", ObjectData::RealPropUnchecked, "MongoDB")->toObject();
+  String db_name = db->o_realProp("db_name", ObjectData::RealPropUnchecked, "MongoDB")->toString();
+  String collection_name = obj->o_realProp("name", ObjectData::RealPropUnchecked, "MongoCollection")->toString();
+
+  collection = mongoc_client_get_collection (get_client(client)->get(), db_name.c_str(), collection_name.c_str());
+  return collection;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // class MongoCollection
 
@@ -70,8 +82,27 @@ static Array HHVM_METHOD(MongoCollection, getIndexInfo) {
    *   inserted array is empty). 
    */
 //public function insert(mixed $a, array $options = array()): mixed;
-static Variant HHVM_METHOD(MongoCollection, insert, Variant a, Array options) {  //TODO
-  throw NotImplementedException("Not Implemented");
+static Variant HHVM_METHOD(MongoCollection, insert, Variant a, Array options) {
+  mongoc_collection_t *collection;
+  bson_t doc;
+  bson_error_t error;
+  bson_oid_t oid;
+
+  collection = get_collection(this_);
+
+  Array doc_array = a.toArray();
+  bson_init(&doc);
+  bson_oid_init_from_string(&oid, doc_array[String("_id")].toString().c_str());
+  bson_append_oid(&doc, "_id", 3, &oid);
+  //Supporting only "name" key
+  bson_append_utf8(&doc, "name", 4, doc_array[String("name")].toString().c_str(), doc_array[String("name")].toString().length());
+
+  bool ret = mongoc_collection_insert(collection, MONGOC_INSERT_NONE, &doc, NULL, &error);
+
+  mongoc_collection_destroy (collection);
+  bson_destroy(&doc);
+
+  return ret;
   /*
   bool mongoc_collection_insert (mongoc_collection_t           *collection,
                                 mongoc_insert_flags_t          flags,
@@ -79,15 +110,45 @@ static Variant HHVM_METHOD(MongoCollection, insert, Variant a, Array options) { 
                                 const mongoc_write_concern_t  *write_concern,
                                 bson_error_t                  *error);
   */
-  
-  //auto connection = this_->o_realProp("connection", ObjectData::RealPropUnchecked, "MongoCursor")->toObject();
-  //this_->o_set("started_iterating", Variant(true), "MongoCursor");
 
 }
 
+/**
+   * Remove records from this collection
+   *
+   * @param array $criteria - criteria    Description of records to
+   *   remove.
+   * @param array $options - options    Options for remove.    "justOne"
+   *    Remove at most one record matching this criteria.
+   *
+   * @return bool|array - Returns an array containing the status of the
+   *   removal if the "w" option is set. Otherwise, returns TRUE.
+   */
 //public function remove(array $criteria = array(), array $options = array()): mixed;
-static Variant HHVM_METHOD(MongoCollection, remove, Array criteria, Array options) {   //TODO
-  throw NotImplementedException("Not Implemented");
+static Variant HHVM_METHOD(MongoCollection, remove, Array criteria, Array options) {
+  mongoc_collection_t *collection;
+  bson_t criteria_b;
+  bson_error_t error;
+
+  collection = get_collection(this_);
+  
+  bson_init(&criteria_b);
+  //Supporting only "name" key
+  bson_append_utf8(&criteria_b, "name", 4, criteria[String("name")].toString().c_str(), criteria[String("name")].toString().length());
+
+  bool ret = mongoc_collection_delete(collection, MONGOC_DELETE_NONE, &criteria_b, NULL, &error);
+
+  mongoc_collection_destroy (collection);
+  bson_destroy(&criteria_b);
+
+  return ret;
+  /*
+  bool mongoc_collection_delete (mongoc_collection_t           *collection,
+                                mongoc_delete_flags_t          flags,
+                                const bson_t                  *selector,
+                                const mongoc_write_concern_t  *write_concern,
+                                bson_error_t                  *error);
+  */
 }
 
 //public function save(mixed $a, array $options = array()): mixed;
--- test/mongoCollectionTest.php
+++ test/mongoCollectionTest.php
@@ -5,10 +5,25 @@ class MongoCollectionTest extends MongoTestCase {
 	public function testMongoCollection() {
 		printf("Running testMongoCollection\n");
 		$cli = new MongoClient();
-		$database_name = "Fake database";
+		$database_name = "test";
 		$db = new MongoDB($cli, $database_name);
-		$coll_name = "Fake collection";
+		$coll_name = "students";
 		$coll = new MongoCollection($db, $coll_name);
+
+		//TODO: add test case for insert
+		$new_doc = array("_id"=>"123456781234567812345678", "name" => "Dan"); //24-digit _id required
+		$this->assertEquals(true, $coll->insert($new_doc));
+		$cursor = $coll->find(array("name" => "Dan"));
+		$cursor->rewind();
+		while ($cursor->valid())
+		{
+		    //$key = $cursor->key();   //This should be tested after BSON-PHP decoder is finished
+		    //var_dump($key);
+		    $value = $cursor->current();
+		    var_dump($value);
+		    $cursor->next();
+		}
+		$this->assertEquals(true, $coll->remove(array("name"=>"Dan")));
 		printf("Ended testMongoCollection\n\n");
 	}
 }
