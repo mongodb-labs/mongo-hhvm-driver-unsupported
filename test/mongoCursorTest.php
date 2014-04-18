c70a14d779f8d1b3830ad5c7d2a5afaeb79fc666
Marco Gentili <mgentili@gmail.com>
2014-04-18 11:11:06 -0700

    Made MongoCursor.cpp use bson_decode for returing results

--- test/mongoCursorTest.php
+++ test/mongoCursorTest.php
@@ -6,29 +6,33 @@ class MongoCursorTest extends MongoTestCase {
 		$cli = new MongoClient();
 		$database_name = "test.students";
 		$cursor = new MongoCursor(	$cli, 
-									$database_name, 
-									array("name" => "Bob"),
-                                  	array()	);
-		//var_dump($cursor->current());
-		//var_dump($cursor->info());
-		//$this->assertEquals(true, $cursor);
-		//var_dump($cursor->batchSize(5));
+									$database_name);
 		
 		$cursor->rewind();
-
-		//Expect to have an exception since the cursor has started iterating.
-		$this->setExpectedException('MongoCursorException');
-		$cursor->addOption("", "");
-		
 		while ($cursor->valid())
 		{
 		    //$key = $cursor->key();   //This should be tested after BSON-PHP decoder is finished
 		    //var_dump($key);
 		    $value = $cursor->current();
-		    //var_dump($value);
+		    var_dump($value);
 		    $cursor->next();
 		}
 		
+	}
+
+	public function testStartedIterating() {
+		$cli = $this->getTestClient();
+		$database_name = "test.students";
+		$cursor = new MongoCursor(	$cli, 
+									$database_name, 
+									array("name" => "Bob"),
+                                  	array()	);
+
+		$cursor->rewind();
+
+		//Expect to have an exception since the cursor has started iterating.
+		$this->setExpectedException('MongoCursorException');
+		$cursor->addOption("", "");
 	}
 
 }
