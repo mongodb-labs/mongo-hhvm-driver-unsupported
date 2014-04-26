d73eb1eeca8f0039424775a27e5094da976ab78a
Jeremy Mikola <jmikola@gmail.com>
2014-04-26 10:06:30 -0500

    Make exception classes available in C++

--- src/ext_mongo.h
+++ src/ext_mongo.h
@@ -4,6 +4,59 @@
 #include "mongo_common.h"
 namespace HPHP {
 
+//////////////////////////////////////////////////////////////////////////////
+// PHP Exceptions and Classes (adapted from Imagick core extension)
+#define MONGO_DEFINE_CLASS(CLS) \
+  class CLS { \
+   public: \
+    static Object allocObject() { \
+      if (cls == nullptr) { \
+        initClass(); \
+      } \
+      return ObjectData::newInstance(cls); \
+    } \
+    \
+    static Object allocObject(const Variant& arg) { \
+      Object ret = allocObject(); \
+      TypedValue dummy; \
+      g_context->invokeFunc(&dummy, \
+                              cls->getCtor(), \
+                              make_packed_array(arg), \
+                              ret.get()); \
+      return ret; \
+    } \
+    \
+   private: \
+    static void initClass() { \
+      cls = Unit::lookupClass(StringData::Make(#CLS)); \
+    } \
+    \
+    static HPHP::Class* cls; \
+  };
+
+MONGO_DEFINE_CLASS(MongoException)
+MONGO_DEFINE_CLASS(MongoConnectionException)
+MONGO_DEFINE_CLASS(MongoCursorTimeoutException)
+MONGO_DEFINE_CLASS(MongoDuplicateKeyException)
+MONGO_DEFINE_CLASS(MongoExecutionTimeoutException)
+MONGO_DEFINE_CLASS(MongoGridFSException)
+MONGO_DEFINE_CLASS(MongoProtocolException)
+MONGO_DEFINE_CLASS(MongoResultException)
+MONGO_DEFINE_CLASS(MongoWriteConcernException)
+
+#undef MONGO_DEFINE_CLASS
+
+template<typename T>
+void mongoThrow(const char* message);
+
+template<typename T>
+void mongoThrow(const char* message) {
+  std::string msg(message);
+  throw T::allocObject(msg);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
 static void mongoc_log_handler(mongoc_log_level_t log_level,
                                const char *log_domain, const char *message,
                                void *user_data) {
